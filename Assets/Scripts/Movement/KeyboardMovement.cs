aeb84124e6bb3f0a2a585721893241cc6b9de2de
Aquarox <bastiendos@gmail.com>
2016-10-28 22:53:04 +0200

    Adding Keyboard Movement

---
+++ Assets/Scripts/Movement/KeyboardMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardMovement : MonoBehaviour {
+
+	public float speed;
+	private Vector2 dir = Vector2.up;
+	private bool horizontal = true;
+	private bool vertical = false;
+
+	void Update(){
+		if (Input.GetKey (KeyCode.RightArrow) && horizontal) {
+			horizontal = false;
+			vertical = true;
+			dir = Vector2.right;
+		}
+		else if (Input.GetKey (KeyCode.DownArrow) && vertical) {
+			horizontal = true;
+			vertical = false;
+			dir = -Vector2.up;
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow) && horizontal) {
+			horizontal = false;
+			vertical = true;
+			dir = -Vector2.right;
+		}
+		else if (Input.GetKey (KeyCode.UpArrow) && vertical) {
+			horizontal = true;
+			vertical = false;
+			dir = Vector2.up;
+		}
+
+		Move ();
+	}
+
+	void Move(){
+		Vector2 pos = transform.position;
+		pos += dir * speed * Time.deltaTime;
+		transform.position = pos;
+	}
+}
