d01aafcb5285c42946af2c6173a89f582e581bfa
Aquarox <bastiendos@gmail.com>
2016-11-26 11:57:29 +0100

    Adding collision with tails and border

---
+++ Assets/Scripts/CollisionBorder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionBorder : MonoBehaviour {
+	
+	public GameObject toDestroy;
+	public string tag;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.CompareTag (tag)) {
+			Destroy (toDestroy);
+			Debug.Log("GAME OVER");
+		}
+
+	}
+}
---
+++ Assets/Scripts/CollisionTail.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTail : MonoBehaviour {
+
+	public GameObject toDestroy;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.CompareTag (gameObject.tag)) {
+			Destroy (toDestroy);
+			Debug.Log("GAME OVER");
+		}
+	}
+}
