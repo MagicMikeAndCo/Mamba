b97bc39c9112c49cf78a94c26d0aaf0f5b2f26a1
Aquarox <bastiendos@gmail.com>
2016-11-27 11:36:10 +0100

    Adding music

---
+++ Assets/Scripts/KeepPlaying.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepPlaying : MonoBehaviour {
+
+	private GameObject[] music;
+
+	void Start(){
+		music = GameObject.FindGameObjectsWithTag ("gameMusic");
+		Destroy (music[1]);
+	}
+		
+	void Awake () {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+}
+
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicManager : MonoBehaviour {
+	
+	public List<AudioClip> musics = new List<AudioClip>();
+	public AudioSource audioSource;
+	private AudioClip playing;
+
+	public void OnStart(){
+		SelectMusic ();
+	}
+
+	public void SelectMusic(){
+		bool musicFind = false;
+		AudioClip musicToPlay;
+		while (!musicFind) {
+			if (musics.Count == 1) {
+				musicToPlay = musics [0];
+			} else {
+				musicToPlay = musics [Random.Range (0, musics.Count - 1)];
+				if (musicToPlay != playing) {
+					musicFind = true;
+					playing = musicToPlay;
+				}
+			}
+		}
+	}
+
+	public void PlayMusic(){
+		audioSource.PlayOneShot (playing);
+	}
+}
